b0252b26263689f58edb433672d39295421786cf
Kailani Garcia <kaigarcia2005@gmail.com>
2026-05-24 00:52:16 +0000

    lab1ex2

---
+++ lab1_ex2_input.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main(void) {
+    int age;
+    char initial;
+    float decimal;
+
+
+    printf("Enter your age (1-120): ");
+    fscanf(stdin, "%d", &age);
+    do {
+        printf("Invalid input. Try again.\n");
+        printf("Enter your age (1-120): ");
+        fscanf(stdin, "%d", &age);
+    }while (age < 1 || age > 120);
+
+
+    printf("Enter you initial (A-Z): ");
+    fscanf(stdin, " %c", &initial);
+    do {
+        printf("Invalid input. Try again.\n");
+        printf("Enter you initial (A-Z): ");
+        fscanf(stdin, " %c", &initial);
+    }while ((initial < 'a' || initial > 'z') && (initial < 'A' || initial > 'Z'));
+
+
+    printf("Enter a positive decimal: ");
+    fscanf(stdin, "%f", &decimal);
+    do {
+        printf("Invalid input. Try again.\n");
+        printf("Enter a positive decimal: ");
+        fscanf(stdin, "%f", &decimal);
+    }while (decimal <= 0.0);
+
+    printf("Hello, %c. You are %d years old. Your number: %f", initial, age, decimal);
+}
